4677861a14edbf4a759be795dc4281019b2be54b
Kenan <kenan.genjac7@gmail.com>
2022-01-16 00:20:52 +0100

    sidebar component finished

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -3,7 +3,32 @@ import "./sidebar.css"
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="sidebarItem"> </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img className="sidebarImg" src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime in aut expedita iure laboriosam velit ipsam quia, laborum aliquid pariatur</p>
+            </div>
+            
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Cinema</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                </ul>
+            </div>
+
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fab fa-facebook"></i>
+                    <i className="sidebarIcon fab fa-instagram"></i>
+                    <i className="sidebarIcon fab fa-twitter-square"></i>
+                </div>
+            </div>
         </div>
     )
 }
