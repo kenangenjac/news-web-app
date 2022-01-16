6dff8c62f73d7e7c48c6904f252700759a106600
Kenan <kenan.genjac7@gmail.com>
2022-01-16 20:20:41 +0100

    posts component finished

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,11 +6,11 @@ import "./home.css"
 export default function Home() {
     return (
         <>
-        <Header/>
-        <div className="home">
-            <Posts/>
-            <Sidebar/>
-        </div>
+            <Header/>
+            <div className="home">
+                <Posts/>
+                <Sidebar/>
+            </div>
         </>
     )
 }
