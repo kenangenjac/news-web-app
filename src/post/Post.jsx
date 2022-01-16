3bfe19e429a655e455160fb4e72c182944c1bbaf
Kenan <kenan.genjac7@gmail.com>
2022-01-16 21:04:35 +0100

    posts resizing bug fixed

---
+++ src/post/Post.jsx
@@ -0,0 +1,20 @@
+import "./post.css"
+
+export default function Post() {
+    return (
+        <div className="post">
+            <img src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="postImg" />
+            <div className="postInfo">
+                <div className="postCategories">
+                    <span className="postCategory">Music</span>
+                    <span className="postCategory">Lifestyle</span>
+                </div>
+                <span className="postTitle">Lorem ipsum dolor sit, amet.</span>
+                <span className="postDate">1 hour ago</span>
+            </div>
+            <p className="postDesc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui, consequuntur amet architecto enim, laboriosam est doloremque excepturi voluptas veniam nisi quisquam nostrum tempore sint accusantium, ducimus illo recusandae aliquam in neque iste reiciendis? Rerum nihil aliquam impedit, ab exercitationem deserunt. Veniam eum aperiam, temporibus delectus iste repudiandae recusandae illum nesciunt doloribus. Perspiciatis quo assumenda soluta itaque nesciunt enim sit laudantium eos? Iste repellat inventore quam animi cum architecto necessitatibus dolor quisquam voluptates doloribus, itaque quaerat asperiores soluta earum, tempore rem enim quo, voluptas placeat! Impedit quas fuga iure quaerat velit quo sint tempore autem voluptatibus eius esse vero vitae ipsa amet quam debitis officiis, sapiente voluptates. Temporibus molestiae optio, praesentium voluptatem nemo aperiam harum cupiditate libero necessitatibus. Quam sit, quo doloremque consequatur nostrum eum in deserunt tenetur! Minima exercitationem et commodi cum dolor repudiandae repellat placeat, vel recusandae itaque, praesentium provident eos iusto molestiae voluptatibus quasi quas laborum eius animi tempora nam, earum mollitia adipisci? At amet cum minus perferendis molestias ratione consectetur aperiam eum omnis inventore odio, non adipisci consequuntur quas quaerat suscipit perspiciatis quidem cupiditate vel aspernatur illo, in maxime sequi labore. Vel explicabo velit expedita eos voluptate perferendis, aut voluptates sunt labore nihil consectetur id nesciunt!
+            </p>
+        </div>
+    )
+}
--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -1,51 +1,16 @@
+import Post from "../post/Post"
 import "./posts.css"
 
 export default function Posts() {
     return (
         <div className="posts">
-            <div className="postsItem">
-                <img className="postsImg" src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-                <div className="postCategories">
-                    <span className="postCategory">Music</span>
-                    <span className="postCategory">Lifestyle</span>
-                </div>
-                <h1 className="postHeading">Lorem ipsum dolor sit amet</h1>
-                <span className="postTime">4 hours ago</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ducimus nostrum architecto qui id maiores harum. Quaerat modi soluta deleniti. Neque fugiat dolorum hic illo maxime, harum molestias molestiae doloribus!</p>
-            </div>
-
-            <div className="postsItem">
-                <img className="postsImg" src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-                <div className="postCategories">
-                    <span className="postCategory">Music</span>
-                    <span className="postCategory">Lifestyle</span>
-                </div>
-                <h1 className="postHeading">Lorem ipsum dolor sit amet</h1>
-                <span className="postTime">4 hours ago</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ducimus nostrum architecto qui id maiores harum. Quaerat modi soluta deleniti. Neque fugiat dolorum hic illo maxime, harum molestias molestiae doloribus!</p>
-            </div>
-
-            <div className="postsItem">
-                <img className="postsImg" src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-                <div className="postCategories">
-                    <span className="postCategory">Music</span>
-                    <span className="postCategory">Lifestyle</span>
-                </div>
-                <h1 className="postHeading">Lorem ipsum dolor sit amet</h1>
-                <span className="postTime">4 hours ago</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ducimus nostrum architecto qui id maiores harum. Quaerat modi soluta deleniti. Neque fugiat dolorum hic illo maxime, harum molestias molestiae doloribus!</p>
-            </div>
-
-            <div className="postsItem">
-                <img className="postsImg" src="https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-                <div className="postCategories">
-                    <span className="postCategory">Music</span>
-                    <span className="postCategory">Lifestyle</span>
-                </div>
-                <h1 className="postHeading">Lorem ipsum dolor sit amet</h1>
-                <span className="postTime">4 hours ago</span>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ducimus nostrum architecto qui id maiores harum. Quaerat modi soluta deleniti. Neque fugiat dolorum hic illo maxime, harum molestias molestiae doloribus!</p>
-            </div>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
         </div>
     )
 }
