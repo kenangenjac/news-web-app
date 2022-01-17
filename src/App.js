0390edcf7f8146a8267d870e9ab310053d12b684
Kenan <kenan.genjac7@gmail.com>
2022-01-17 15:05:05 +0100

    login page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import TopBar from "./components/topbar/TopBar";
 import Settings from "./pages/settings/Settings";
-import SinglePage from "./pages/SinglePage/SinglePage";
+import SinglePage from "./pages/singlePage/SinglePage";
 import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <Settings />
+      <Login />
     </div>
   );
 }
