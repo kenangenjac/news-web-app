efbc584160d44f6268dc51421553bf7e23d61ef6
Kenan <kenan.genjac7@gmail.com>
2022-01-19 21:05:10 +0100

    user authentification finished

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 
 const app = express();
-app.use(express.json()); //for sending json objects
+app.use(express.json()); //for enabling of sending json objects
 
 dotenv.config();
 const URI = process.env.MONGO_URL;
@@ -17,7 +17,8 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.use("/server/auth", authRoute); // indicates path for posting
+//route corresponds to the route of the post req
+app.use("/api/auth", authRoute); // indicates path for posting
 
 app.get("/", (req, res) => {
   res.send("Kenans");
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bycrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
+// registration
 router.post("/register", async (req, res) => {
   try {
-    const salt = await bycrypt.genSalt(10);
-    const hashedPass = await bycrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
 
     const newUser = new User({
       username: req.body.username,
@@ -13,8 +14,29 @@ router.post("/register", async (req, res) => {
       password: hashedPass,
     });
 
-    const user = await newUser.save();
+    const user = await newUser.save(); //mongo method to save user to db
     res.status(200).json(user);
+    // console.log(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username }); //querrying unique user from db
+    !user && res.status(400).json("Wrong credentials");
+
+    const validatePassword = await bcrypt.compare(
+      //comparing entered and hashed pass
+      req.body.password,
+      user.password
+    );
+    !validatePassword && res.status(400).json("Wrong credentials");
+
+    const { password, ...others } = user._doc; //excluding password from user when printing it
+    res.status(200).json(others);
   } catch (error) {
     res.status(500).json(error);
   }
