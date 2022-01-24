4362200cd52829509208c92692030af9c9a24f0c
Kenan <kenan.genjac7@gmail.com>
2022-01-24 11:05:16 +0100

    routed image uploading

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const imageRoute = require("./routes/images");
 
 dotenv.config();
 app.use(express.json()); //for enabling of sending json objects
@@ -22,26 +23,11 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-// it takes the file and saves inside images file, and filename is the name we are providing on the client side
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images"); // second parameter is destination
-  },
-  filename: (req, file, callback) => {
-    callback(null, "biggie.jpg");
-  },
-});
-
-const upload = multer({ storage: storage });
-//upload.single("name"), name must be the same as the key i.e. the key of the input name in form
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
-});
-
 app.use("/api/auth", authRoute); //mounting routes in auth router on /api/auth
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+app.use("/api/upload", imageRoute);
 
 app.get("/", (req, res) => {
   res.send("Kenans");
