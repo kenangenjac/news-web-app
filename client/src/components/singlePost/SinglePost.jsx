abbfe4f65eb0bcfd8f3d7ba96830be0d22c05dc2
Kenan <kenan.genjac7@gmail.com>
2022-01-28 19:41:17 +0100

    post updating finished

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -12,10 +12,17 @@ export default function SinglePost() {
     const publicFolder = "http://localhost:5000/images/";
     const { user } = useContext(Context);
 
+    //  states for updating a post
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [updateMode, setUpdateMode] = useState(false)
+
     useEffect(()=>{
         const getPost = async ()=>{
             const res = await axios.get("/posts/" + postId);
             setPost(res.data)
+            setTitle(res.data.title)    // setting values so i use them in editing
+            setDescription(res.data.description)
             // console.log(res.data)
         }
         getPost()
@@ -32,7 +39,19 @@ export default function SinglePost() {
         }
     }
 
-    const handleEdit = async () => {}
+    const handleUpdate = async () => {
+        try {
+            await axios.put(`/posts/${post._id}`, {
+                    username: user.username,
+                    title,
+                    description
+                }
+            )
+            setUpdateMode(false)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <div className="singlePost">
@@ -40,15 +59,23 @@ export default function SinglePost() {
                 {
                     post.photo && (<img src={publicFolder + post.photo} alt="" className="singlePostImg" />)
                 }
-                <h1 className="singlePostTitle">
-                    {post.title}
-                    {post.username === user?.username && ( //user?.username - if there is no user, its not going to look for this username
-                        <div className="singlePostEdit">
-                            <i className="singlePostIcon fas fa-edit" onClick={handleEdit}></i>
-                            <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
-                        </div>
-                    )}           
-                </h1>
+                {
+                    updateMode ? 
+                    ( <h1 className="singlePostTitle">Title: <input type="text" className="singlePostTitle singlePostTitleInput" autoFocus value={title} onChange={e=>{setTitle(e.target.value)}}/></h1> ) : 
+                    (
+                        <h1 className="singlePostTitle">
+                            {title}
+                            {post.username === user?.username && //user?.username - if there is no user, its not going to look for this username
+                            ( 
+                                <div className="singlePostEdit">
+                                    <i className="singlePostIcon fas fa-edit" onClick={() => setUpdateMode(true) }></i>
+                                    <i className="singlePostIcon far fa-trash-alt" onClick={ handleDelete }></i>
+                                </div>
+                            )}           
+                        </h1>
+                    )
+                }
+                
                 <div className="singlePostInfo">
                     <span className="singlePostAuthor">
                         Author: 
@@ -58,9 +85,22 @@ export default function SinglePost() {
                     </span>
                     <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
-                <p className="singlePostDesc">
-                    {post.description}
-                </p>
+                {
+                    updateMode ? (
+                        <div>
+                            <h1 className="singlePostTitle singlePostTitleUpdate">Description: </h1> 
+                            <div className="writeFormGroupUpdate">
+                                <textarea value={description} type="text" className="writeInput writeText" placeholder="Update Your story..." onChange={e=>setDescription(e.target.value)}></textarea>
+                            </div>
+                        </div>
+                    ) : 
+                    (
+                        <p className="singlePostDesc">
+                            {description}
+                        </p>
+                   )
+                }
+                {updateMode && <button className="singlePostButton" onClick={handleUpdate}>Update</button>} 
             </div>
         </div>
     )
